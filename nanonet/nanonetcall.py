aad6c1d0af31d366ce2c8529fcfee6d29690e3cb
Chris Wright <chris.wright@nanoporetech.com>
2016-03-14 14:26:37 +0000

    Remove old C decoding and currennt based basecaller
    Remove deprecated setuptools ext

--- nanonet/nanonetcall.py
+++ nanonet/nanonetcall.py
@@ -11,12 +11,14 @@ import timeit
 import subprocess
 import pkg_resources
 
-from nanonet import run_currennt
+import numpy as np
+
+from nanonet import decoding, nn
 from nanonet.fast5 import Fast5, iterate_fast5
-from nanonet.util import random_string, conf_line, FastaWrite, tang_imap
+from nanonet.util import random_string, conf_line, FastaWrite, tang_imap, all_nmers, kmers_to_sequence
 from nanonet.cmdargs import FileExist, CheckCPU, AutoBool
 from nanonet.parse_currennt import CurrenntParserCaller
-from nanonet.features import make_currennt_basecall_input_multi
+from nanonet.features import make_basecall_input_multi
 
 import warnings
 warnings.simplefilter("ignore")
@@ -39,8 +41,6 @@ def get_parser():
         help="List of reads to process.")
     parser.add_argument("--limit", default=None, type=int,
         help="Limit the number of input for processing.")
-    parser.add_argument('--workspace', default=None,
-        help='Workspace directory')
     parser.add_argument("--min_len", default=500, type=int,
         help="Min. read length (events) to basecall.")
     parser.add_argument("--max_len", default=15000, type=int,
@@ -49,71 +49,65 @@ def get_parser():
     parser.add_argument("--model", type=str, action=FileExist,
         default=pkg_resources.resource_filename('nanonet', 'data/template_model.jsn'),
         help="Trained ANN.")
-    parser.add_argument("--device", type=int, default=0,
-        help="ID of CUDA device to use." )
-    parser.add_argument("--cuda", default=False, action=AutoBool,
-        help="Use CPU for neural network calculations.")
-    parser.add_argument("--network_jobs", default=1, type=int, action=CheckCPU,
-        help="No of neural network jobs to run in parallel, only valid with --no-cuda.")
     parser.add_argument("--decoding_jobs", default=1, type=int, action=CheckCPU,
-        help="No of Viterbi decoding jobs to run in parallel.")
-    parser.add_argument("--nseqs", default=1, type=int,
-        help="No. of sequences for currennt to process simultaneously. The upper limit is determined by --max_len and ANN size." )
-    parser.add_argument("--batch", default=None, type=int,
-        help="No. of sequences to include in each processing batch.")
+        help="No of decoding jobs to run in parallel.")
 
-    parser.add_argument("--trans", type=float, nargs='+', default=[0.1153, 0.6890, 0.1881, 0.0077],
-        help="Transition parameters, stay, step1, step2 and step3, to enable this, use --use_trans.")
+    parser.add_argument("--trans", type=float, nargs=3, default=None,
+        metavar=('stay', 'step', 'skip'), help='Base transition probabilities')
     parser.add_argument("--window", type=int, nargs='+', default=[-1, 0, 1],
         help="The detailed list of the entire input window, default -1 0 1, tested with R7.3 to be optimal.")
-    parser.add_argument("--trans_free", action=AutoBool, default=True,
-        help="Estimate transition parameters by magic, the magic may suffer from some numerical issues.")
-    parser.add_argument("--use_trans", dest="trans_free", action="store_false",
-        help="Use fixed input transition parameters, no magic.")
-    parser.add_argument("--cache_path", default=tempfile.gettempdir(),
-        help="Path for currennt cache files.")
 
     return parser
 
 
-def process_reads(workspace, modelfile, cache_path, device, cuda, nseqs, inputfile, **kwargs):
+def process_read(modelfile, fast5, min_prob=1e-5, trans=None, **kwargs):
     """Run neural network over a set of fast5 files
 
-    :param workspace: directory in which to write intermediates and neural
-        network output.
-    :param: modelfile: neural network specification.
-    :param cache_path: cache path for currennt.
-    :param device: ID of CUDA device to use.
-    :param cuda: use cuda?
-    :param nseqs: no. of sequences to process in parallel.
+    :param modelfile: neural network specification.
+    :param fast5: read file to process
+    :param **kwargs: kwargs of make_basecall_input_multi
     """
+    kmer_len = 3 #TODO: parameterise this
 
-    batch, fast5s, netcdf = inputfile
-    reads_written = make_currennt_basecall_input_multi(fast5s, netcdf_file=netcdf, **kwargs)
-    if reads_written == 0:
-        sys.stderr.write('All reads filtered out in batch {}.\n'.format(batch))
-        return batch, None
+    t0 = timeit.default_timer()
+    results = list(make_basecall_input_multi((fast5,), **kwargs))
+    t1 = timeit.default_timer()
+    feature_time = t1 - t0
 
-    # Currennt config file
-    currennt_cfg = os.path.join(workspace, 'currennt_{}.cfg'.format(batch))
-    currennt_out = os.path.join(workspace, 'currennt_{}.out'.format(batch))
+    for name, features in results:
+        t0 = timeit.default_timer()
+        network = np.load(modelfile).item()
+        t1 = timeit.default_timer()
+        post = network.run(features.astype(nn.tang_nn_type))
+        t2 = timeit.default_timer()
 
-    with open(currennt_cfg, 'w') as cfg:    
-        cfg.write(conf_line('network', modelfile))
-        cfg.write(conf_line('ff_input_file', netcdf))
-        cfg.write(conf_line('ff_output_file', currennt_out))
-        cfg.write(conf_line('ff_output_format', 'single_csv'))
-        cfg.write(conf_line('input_noise_sigma', 0.0))
-        cfg.write(conf_line('parallel_sequences', nseqs))
-        cfg.write(conf_line('cache_path', cache_path))
-        if not cuda:
-            cfg.write(conf_line('cuda', 'false'))   
- 
-    # Run Currennt
-    run_currennt(currennt_cfg, device)
-    os.unlink(netcdf)
-    sys.stderr.write('Finished neural network processing for batch {}.\n'.format(batch))
-    return batch, currennt_out
+        # Reorder ATGC -> ACGT, models are trained with funny order
+        kmers_nn = all_nmers(kmer_len)
+        kmers_nn_revmap = {k:i for i, k in enumerate(kmers_nn)}
+        kmers_hmm = sorted(kmers_nn)
+        nkmers = len(kmers_nn)
+        kmer_out_order = np.arange(nkmers)
+        kmer_order = np.fromiter(
+            (kmers_nn_revmap[k] for k in kmers_hmm),
+            dtype=int, count=len(kmers_hmm)
+        )
+
+        # Strip out events where XXX most likely, and XXX states entirely 
+        max_call = np.argmax(post, axis=1)
+        post = post[max_call < nkmers]
+        post = post[:, :-1]
+        post[:, kmer_out_order] = post[:, kmer_order]
+        post /= np.sum(post, axis=1).reshape((-1, 1))
+
+        post = min_prob + (1.0 - min_prob) * post
+        trans = decoding.estimate_transitions(post, trans=trans)
+        score, states = decoding.decode_profile(post, trans=trans, log=False)
+
+        kmer_path = [kmers_hmm[i] for i in states]
+        seq = kmers_to_sequence(kmer_path)
+        t3 = timeit.default_timer()
+
+        yield name, seq, score, len(post), (feature_time, t1 - t0, t2 - t1, t3 - t2)
 
 
 def main():
@@ -129,44 +123,8 @@ def main():
             print "No 'section' found in modelfile, try specifying --section."
             sys.exit(1)
 
-    if args.cuda:
-        args.network_jobs = 1
-    else:
-        args.nseqs = 1
-
-    # User-defined workspace or use system tmp, if using tmp
-    #   clean up as we go since the workspace can get large
-    clean_workspace = False
-    workspace = args.workspace
-    if workspace is None:
-        clean_workspace = True
-        workspace = os.path.join(tempfile.gettempdir(), random_string())
-    if not os.path.exists(workspace):
-        os.makedirs(workspace)
-
-    # Define currennt input(s)
-    inputfile_tmpl = os.path.join(workspace, 'basecall_features_{}.netcdf')
-    sys.stderr.write("Creating currennt input NetCDF(s): {}\n".format(inputfile_tmpl))
-    fast5_files = list(iterate_fast5(args.input, paths=True, strand_list=args.strand_list, limit=args.limit))
-    if args.batch is None:
-        batch_size = int(math.ceil(float(len(fast5_files)) / args.network_jobs))
-    else:
-        batch_size = args.batch
-    inputs = []
-    for i, group in enumerate(fast5_files[i:i+batch_size] for i in xrange(0, len(fast5_files), batch_size)):
-        inputfile = inputfile_tmpl.format(i)
-        inputs.append((i, group, os.path.abspath(inputfile)))         
-
-    sys.stderr.write("Running basecalling for {} sections. Network is running on {} {}(s) with"
-        " decoding running on {} CPU(s). Batch size is {}.\n".format(
-        args.section, args.network_jobs, 'GPU' if args.cuda else 'CPU',
-        args.decoding_jobs, batch_size)
-    )
-
     fix_args = [
-        workspace, modelfile, args.cache_path,
-        args.device, args.cuda,
-        args.nseqs,
+        modelfile
     ]
     fix_kwargs = {
         'window':args.window,
@@ -175,33 +133,21 @@ def main():
         'section':args.section
     }
 
-    pstay  = args.trans[0]
-    pstep1 = args.trans[1]/4.0
-    pstep2 = args.trans[2]/16.0
-    pstep3 = args.trans[3]/44.0
-
     t0 = timeit.default_timer()
     n_reads = 0
     n_bases = 0
+    timings = [0, 0, 0, 0]
+    fast5_files = list(iterate_fast5(args.input, paths=True, strand_list=args.strand_list, limit=args.limit))
     with FastaWrite(args.output) as fasta:
-        for batch, currennt_out in tang_imap(process_reads, inputs, fix_args=fix_args, fix_kwargs=fix_kwargs, threads=args.network_jobs):
-            if currennt_out == None:
-                continue
-            # Viterbi calls
-            cpc = CurrenntParserCaller(
-                fin=currennt_out, trans_free=args.trans_free,
-                pstay=pstay, pstep1=pstep1, pstep2=pstep2, pstep3=pstep3
-            )
-            for result in cpc.basecalls(ncpus=args.decoding_jobs):
-                fasta.write(*result)
+        for results in tang_imap(process_read, fast5_files, fix_args=fix_args, fix_kwargs=fix_kwargs):
+            for name, basecall, _, _, time in results:
+                fasta.write(*(name, basecall))
                 n_reads += 1
-                n_bases += len(result[1])
-            if clean_workspace:
-                os.unlink(currennt_out)
-                
-            sys.stderr.write('Finished basecalling batch {}.\n'.format(batch))
+                n_bases += len(basecall)
+                timings = [x + y for x, y in zip(timings, time)]              
     t1 = timeit.default_timer()
     sys.stderr.write('Processed {} reads ({} bases) in {}s\n'.format(n_reads, n_bases, t1 - t0))
+    sys.stderr.write('Feature generation: {}\nLoad network: {}\nRun network: {}\nDecoding: {}\n'.format(*timings))
 
 
 if __name__ == "__main__":
--- setup.py
+++ setup.py
@@ -13,24 +13,6 @@ All rights reserved; (c)2016: Oxford Nanopore Technologies, Limited
 *********************************************************************************
 """
 
-
-class EnsureClibs(Command):
-    description = 'Ensures C libraries are precompiled and in data folder.'
-    user_options = []
-    def initialize_options(self):
-        self.cwd = None
-    def finalize_options(self):
-        self.cwd = os.getcwd()
-    def run(self):
-        assert os.getcwd() == self.cwd, 'Must be in package root'
-        # Get all extensions, and check for presence of file
-        for ext in self.__dict__['distribution'].__dict__['ext_modules']:
-            name = os.path.join('nanonet', 'data', '{}.dll'.format(ext.name))
-            if not os.path.isfile(name):
-                raise IOError('Library {} not found, see README.md for details of precompiling libraries.'.format(name))
-        # Remove extensions so we don't try to compile
-        self.__dict__['distribution'].__dict__['ext_modules'] = []
-
 # Get the version number from __init__.py
 verstrline = open(os.path.join('nanonet', '__init__.py'), 'r').read()
 vsre = r"^__version__ = ['\"]([^'\"]*)['\"]"
@@ -40,22 +22,7 @@ if mo:
 else:
     raise RuntimeError('Unable to find version string in "dragonet/__init__.py".')
     
-
-c_compile_args = [
-    '-Wall', '-DNDEBUG', '-std=c99',
-    '-fstrict-aliasing', '-O3', '-march=native'
-]
-
-nanonet_dir = os.path.join(os.path.dirname(__file__), 'nanonet')
-
 extensions = []
-extensions.append(Extension(
-    'clib_viterbi',
-    include_dirs=[nanonet_dir],
-    sources=[os.path.join(nanonet_dir, 'c_log_viterbi.c')],
-    extra_compile_args=c_compile_args
-))
-
 requires=[
     'h5py',
     'numpy',
@@ -65,7 +32,6 @@ extra_requires = {
     'currennt': ['netCDF4']
 }
 
-
 setup(
     name='nanonet',
     version=version,
@@ -73,7 +39,6 @@ setup(
     maintainer='Chris Wright',
     maintainer_email='chris.wright@nanoporetech.com',
     url='http://www.nanoporetech.com',
-    cmdclass={'noext':EnsureClibs},
     packages=find_packages(exclude=["*.test", "*.test.*", "test.*", "test"]),
     package_data={'nanonet.data':['nanonet/data/*']},
     include_package_data=True,
