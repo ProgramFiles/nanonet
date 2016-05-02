77f0d78d2889e642680e96b65e7975b11cb38500
cwright <chris.wright@nanoporetech.com>
2016-05-02 20:21:02 +0100

    Fix up watcher

--- nanonet/watcher.py
+++ nanonet/watcher.py
@@ -11,7 +11,7 @@ except ImportError:
 class Fast5Watcher(object):
 
     def __init__(self, path, timeout=10, regex='.*\.fast5$'):
-        """Watch a path an yield modified files
+        """Watch a path and yield modified files
 
         :param path: path to watch for files.
         :param timeout: timeout period for newly modified files.
@@ -19,12 +19,13 @@ class Fast5Watcher(object):
         """
         self.path = path
         self.timeout = timeout
+        self.regex = regex
         self.q = Queue()
         self.watcher = Process(target=self._watcher)
         self.yielded = set()
 
     def _watcher(self):
-        handler = RegexMatchingEventHandler(regexes=['.*\.fast5$'], ignore_directories=True)
+        handler = RegexMatchingEventHandler(regexes=[self.regex], ignore_directories=True)
         handler.on_modified = lambda x: self.q.put(x.src_path)
         observer = Observer()
         observer.schedule(handler, self.path)
@@ -36,25 +37,15 @@ class Fast5Watcher(object):
             observer.stop()
         observer.join()
 
-    def fast5_collector(self):
-        while True:
-            try:
-                f = self.q.get(self.timeout)
-                if f not in self.yielded:
-                    yield f
-                    self.yielded.add(f)
-            except:
-                break
-
     def __iter__(self):
         self.watcher.start()
         while True:
             try:
-                item = self.q.get(self.timeout)
+                item = self.q.get(True, self.timeout)
             except:
                 break
             else:
                 if item not in self.yielded:
                     yield item
                     self.yielded.add(item)
-        self.watcher.join()
+        self.watcher.terminate()
