4070eb47b782e1ba6e6bb7571c93524c943ee351
Chris Wright <Chris Wright>
2016-06-11 00:00:14 +0100

    Get JobQueue working on windows

--- nanonet/jobqueue.py
+++ nanonet/jobqueue.py
@@ -1,5 +1,4 @@
 from uuid import uuid4
-import platform
 from time import sleep
 import os
 from multiprocessing import Process
@@ -11,74 +10,25 @@ from myriad.managers import make_client
 
 from nanonet.util import stderr_redirected
 
-def times_two(x):
-    return x, 2*x
-
-def times_three(x):
-    return x, 3*x
-
-def multi_times_two(x):
-    return [(y, 2*y) for y in x]
 
 class JobQueue(object):
 
     def __init__(self, jobs, functors):
-        """Watch a path and yield modified files
+        """A simple job queue which can be processed by various functors.
 
+        :param jobs: iterable of job items.
+        :param functions: tuples of the form (function, n_items), if n_itmes
+            is None then the function should accept a single job items and
+            process it to produce a single result. if n_items >= 2, then
+            function should process a list of items, returning a list of
+            results.
         """
         self.jobs = jobs
         self.functors = functors
 
-    def _worker(self, function, take_n, timeout=0.5):
-        sleep(2) # nasty, allows all workers to come up before iteration begins
-        if take_n is None:
-            self._singleton_worker(function, timeout=timeout)
-        else:
-            self._multi_worker(function, take_n, timeout=timeout)
-
-
-    def _singleton_worker(self, function, timeout=0.5):
-        manager = make_client(self.hostname, self.port, self.authkey)
-        job_q = manager.get_job_q()
-        job_q_closed = manager.q_closed()
-        result_q = manager.get_result_q()
-
-        while True:
-            try: 
-                job = job_q.get_nowait()
-                result = function(job)
-                result_q.put(result)
-            except Queue.Empty:
-                if job_q_closed._getvalue().value:
-                    break
-            sleep(timeout)
-
-    def _multi_worker(self, function, take_n, timeout=0.5):
-        manager = make_client(self.hostname, self.port, self.authkey)
-        job_q = manager.get_job_q()
-        job_q_closed = manager.q_closed()
-        result_q = manager.get_result_q()
-
-        while True:
-            jobs = []
-            try:
-                for _ in xrange(take_n):
-                    job = job_q.get_nowait()
-                    jobs.append(job)
-            except Queue.Empty:
-                if job_q_closed._getvalue().value:
-                    break
-            else:
-                for i, res in enumerate(function(jobs)):
-                    result_q.put(res)
-            sleep(timeout)
-        if len(jobs) > 0:
-            for i, res in enumerate(function(jobs)):
-                result_q.put(res)
-
     def __iter__(self):
         self.start_server()
-        workers = [Process(target=partial(self._worker, f[0], f[1])) for f in self.functors]
+        workers = [Process(target=partial(worker, f[0], f[1], self.port, self.authkey)) for f in self.functors]
 
         try:
             for w in workers:
@@ -97,7 +47,6 @@ class JobQueue(object):
 
     def start_server(self, ports=(5000,6000)):
         self.authkey = str(uuid4())
-        self.hostname = platform.uname()[1]
 
         server = None
         for port in xrange(*ports):
@@ -113,3 +62,59 @@ class JobQueue(object):
 
         self.server = server
         self.port = port
+
+
+# On *nix the following could be part of the class above, but not on windows:
+#    https://docs.python.org/2/library/multiprocessing.html#windows
+
+def worker(function, take_n, port, authkey, timeout=0.5):
+    """Worker function for JobQueue. Dispatches to singleton_worker or
+    multi_worker as appropriate.
+
+    :param function: function to apply in job items.
+    :param take_n: number of items to process, should be None or >=2. Special
+        case of None indicates function takes a single item to produce a single
+        result.
+    """
+    sleep(2) # nasty, allows all workers to come up before iteration begins
+    manager = make_client('localhost', port, authkey)
+    job_q = manager.get_job_q()
+    job_q_closed = manager.q_closed()
+    result_q = manager.get_result_q()
+
+    if take_n is None:
+        _singleton_worker(function, job_q, job_q_closed, result_q, timeout=timeout)
+    else:
+        multi_worker(function, take_n, job_q, job_q_closed, result_q, timeout=timeout)
+
+
+def _singleton_worker(function, job_q, job_q_closed, result_q, timeout=0.5):
+    while True:
+        try:
+            job = job_q.get_nowait()
+            result = function(job)
+            result_q.put(result)
+        except Queue.Empty:
+            if job_q_closed._getvalue().value:
+                break
+        sleep(timeout)
+
+
+def _multi_worker(function, take_n, job_q, job_q_closed, result_q, timeout=0.5):
+    while True:
+        jobs = []
+        try:
+            for _ in xrange(take_n):
+                job = job_q.get_nowait()
+                jobs.append(job)
+        except Queue.Empty:
+            if job_q_closed._getvalue().value:
+                break
+        else:
+            for i, res in enumerate(function(jobs)):
+                result_q.put(res)
+        sleep(timeout)
+    if len(jobs) > 0:
+        for i, res in enumerate(function(jobs)):
+            result_q.put(res)
+
