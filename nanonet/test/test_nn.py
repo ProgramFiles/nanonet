cfb722442648a6e272de3e20c30c006e44179be9
cwright <chris.wright@nanoporetech.com>
2016-07-08 22:58:13 +0100

    Make tests work

--- nanonet/test/test_nn.py
+++ nanonet/test/test_nn.py
@@ -8,6 +8,7 @@ class ANNTest(unittest.TestCase):
     def setUpClass(self):
         print '* ANN'
         np.random.seed(0xdeadbeef)
+        self.prec = 6
         self._NSTEP = 10
         self._NFEATURES = 4
         self._SIZE = 5
@@ -15,19 +16,19 @@ class ANNTest(unittest.TestCase):
         self.W = np.random.normal(size=(self._NFEATURES, self._SIZE))
         self.b = np.random.normal(size=self._SIZE)
         self.x = np.random.normal(size=(self._NSTEP, self._NFEATURES))
-        self.res = self.x.dot(self.W) + self.b
+        self.res = (self.x.dot(self.W) + self.b).astype(nn.dtype)
 
     def test_000_single_layer_linear(self):
         network = nn.FeedForward(self.W, self.b, nn.linear)
         self.assertEqual(network.in_size, self._NFEATURES)
         self.assertEqual(network.out_size, self._SIZE)
-        np.testing.assert_array_equal(network.run(self.x), self.res)
+        np.testing.assert_array_almost_equal(network.run(self.x), self.res)
 
     def test_001_single_layer_tanh(self):
         network = nn.FeedForward(self.W, self.b, nn.tanh)
         self.assertEqual(network.in_size, self._NFEATURES)
         self.assertEqual(network.out_size, self._SIZE)
-        np.testing.assert_array_equal(network.run(self.x), np.tanh(self.res))
+        np.testing.assert_array_almost_equal(network.run(self.x), np.tanh(self.res))
 
     def test_002_parallel_layers(self):
         l1 = nn.FeedForward(self.W, self.b, nn.tanh)
@@ -49,17 +50,17 @@ class ANNTest(unittest.TestCase):
         self.assertEqual(network.in_size, self._NFEATURES)
         self.assertEqual(network.out_size, self._SIZE)
 
-        np.testing.assert_array_equal(network.run(self.x), res)
+        np.testing.assert_array_almost_equal(network.run(self.x), res)
 
     def test_004_reverse(self):
         network1 = nn.FeedForward(self.W, self.b, nn.tanh)
         res1 = network1.run(self.x)
         network2 = nn.Reverse(network1)
         res2 = network2.run(self.x)
-        self.assertEqual(network1.in_size, network2.in_size)
+        self.assertEqual(network1.in_size, network2.in_size,)
         self.assertEqual(network1.out_size, network2.out_size)
 
-        np.testing.assert_array_equal(res1, res2)
+        np.testing.assert_array_equal(res1, res2, self.prec)
 
     def test_005_poormans_birnn(self):
         layer1 = nn.FeedForward(self.W, self.b, nn.tanh)
@@ -67,7 +68,7 @@ class ANNTest(unittest.TestCase):
         network = nn.BiRNN(layer1, layer2)
 
         res = network.run(self.x)
-        np.testing.assert_array_equal(res[:,:self._SIZE], res[:,self._SIZE:])
+        np.testing.assert_array_equal(res[:,:self._SIZE], res[:,self._SIZE:], self.prec)
 
     def test_006_softmax(self):
         network = nn.SoftMax(self.W, self.b)
@@ -81,7 +82,7 @@ class ANNTest(unittest.TestCase):
         network = nn.SimpleRNN(W1, b=self.b, fun=nn.linear)
 
         res = network.run(self.x)
-        np.testing.assert_almost_equal(res, self.res)
+        np.testing.assert_almost_equal(res, self.res, self.prec)
 
     def test_008_rnn_no_input(self):
         W1 = np.random.normal(size=(self._SIZE, self._SIZE))
@@ -89,7 +90,7 @@ class ANNTest(unittest.TestCase):
         network = nn.SimpleRNN(W2, fun=nn.linear)
 
         res = network.run(self.x)
-        np.testing.assert_almost_equal(res, 0.0)
+        np.testing.assert_almost_equal(res, 0.0, self.prec)
 
     def test_009_rnn_no_input_with_bias(self):
         W1 = np.random.normal(size=(self._SIZE, self._SIZE))
@@ -97,10 +98,10 @@ class ANNTest(unittest.TestCase):
         network = nn.SimpleRNN(W2, b=self.b, fun=nn.linear)
 
         res = network.run(self.x)
-        res2 = np.zeros(self._SIZE)
+        res2 = np.zeros(self._SIZE, dtype=nn.dtype)
         for i in xrange(self._NSTEP):
             res2 = res2.dot(W1) + self.b
-            np.testing.assert_almost_equal(res[i], res2)
+            np.testing.assert_allclose(res[i], res2, self.prec)
 
     def test_010_birnn_no_input_with_bias(self):
         W1 = np.random.normal(size=(self._SIZE, self._SIZE))
@@ -110,4 +111,4 @@ class ANNTest(unittest.TestCase):
         network = nn.BiRNN(layer1, layer2)
 
         res = network.run(self.x)
-        np.testing.assert_almost_equal(res[:,:self._SIZE], res[::-1,self._SIZE:])
+        np.testing.assert_almost_equal(res[:,:self._SIZE], res[::-1,self._SIZE:], self.prec)
